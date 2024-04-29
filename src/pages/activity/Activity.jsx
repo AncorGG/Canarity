5fa6f10ee9e3db8ea4337b6c98dcde502370c1e5
AncorGG <ancorgarcia63@gmail.com>
2024-04-29 19:57:47 +0100

    Activity_Update_1

--- src/pages/activity/Activity.jsx
+++ src/pages/activity/Activity.jsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import * as Icon from "react-bootstrap-icons";
 import "./Activity.css"
 
 function Activity() {
@@ -8,6 +9,31 @@ function Activity() {
       <Header />
       <div className="section">
         <h1 className="title">Work in Progress...</h1>
+      </div>
+
+      <div className="activity-list">
+        <div className="activity-list-header">
+          <h5 className="activity-list-title">Activity List</h5>
+          <div className="activity-list-header-icons">
+            <i className="icon-add"><Icon.PlusCircle/></i>
+            <i className="icon-filter"><Icon.Filter/></i>
+          </div>
+        </div>
+        <div className="activity-list-item activity-selected">
+          <i className="icon-check"><Icon.Check2All/></i>
+          <p>Morning Exercise</p>
+          <i className="activity-dots"><Icon.ThreeDots/></i>
+        </div>
+        <div className="activity-list-item">
+          <i className="icon-check"><Icon.Check2All/></i>
+          <p>Morning Exercise</p>
+          <i className="activity-dots"><Icon.ThreeDots/></i>
+        </div>
+        <div className="activity-list-item">
+          <i className="icon-check"><Icon.Check2All/></i>
+          <p>Morning Exercise</p>
+          <i className="activity-dots"><Icon.ThreeDots/></i>
+        </div>
       </div>
       <Footer/>
     </>
